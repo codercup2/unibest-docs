8b1270a38caec4d014951183e18bfb855c67f510
Burt <burt.lai@kupotech.com>
2024-11-16 21:12:09 +0800

    feat: 顶部左上角的版本和首页的star

---
+++ docs/.vitepress/theme/components/NavBarTitleAfter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// TODO: 从远程地址获取版本号
+const version = '2.5.0'
+</script>
+
+<template>
+  <span
+    style="
+      color: green;
+      font-size: 10px;
+      margin-left: 4px;
+      background-color: azure;
+      padding: 0 4px;
+      border-radius: 4px;
+      display: inline-block;
+      font-weight: 700;
+    "
+    >{{ version }}</span
+  >
+</template>
